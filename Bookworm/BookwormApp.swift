962b0be8ffacbb0721c6d66302e7e88559de96d8
murad <muradkv@gmail.com>
2026-06-30 21:29:54 +0300

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by murad on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BookwormView()
+        }
+    }
+}
---
+++ Bookworm/BookwormView.swift
@@ -0,0 +1,24 @@
+//
+//  BookwormView.swift
+//  Bookworm
+//
+//  Created by murad on 30.06.2026.
+//
+
+import SwiftUI
+
+struct BookwormView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BookwormView()
+}
